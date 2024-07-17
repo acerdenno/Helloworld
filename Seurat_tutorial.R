110abcc3e13ea52e610270be5dcb5a54b3c223b5
acerdenno <acerdenno@us.es>
2024-07-17 13:45:53 +0200

    seurat basics qc normalizing data

--- Seurat_tutorial.R
+++ Seurat_tutorial.R
@@ -14,3 +14,21 @@ dense.size <- object.size(as.matrix(pbmc.data))
 dense.size
 sparse.size <- object.size(pbmc.data)
 sparse.size
+> # The [[ operator can add columns to object metadata. This is a great place to stash QC stats
+  > pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+> # Show QC metrics for the first 5 cells
+  > head(pbmc@meta.data, 5)
+> # Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+Warning: Default search for "data" layer in "RNA" assay yielded no results; utilizing "counts" layer instead.
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(pbmc), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
