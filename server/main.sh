ab0ffd8f582f849eb141769161239e657dcc1eff
Juan <39202665+ajuancer@users.noreply.github.com>
2024-07-24 17:57:24 +0200

    allocate resources before running script

--- server/main.sh
+++ server/main.sh
@@ -4,23 +4,36 @@
 export $(grep -v '^#' .env | xargs)
 
 # Step 1: Git pull
-echo "Pulling latest changes from git..."
+echo "===> [1/3] Pull changes from repo"
 git pull
 
 # Step 2: Give execution permission to all .sh files in the directory
-echo "Setting execution permissions for all .sh files..."
+echo "===> [2/3] Set execution permissions"
 chmod +x *.sh
 
-# Step 3: Run the script specified in the SCRIPT environment variable
-echo "Running the script specified in SCRIPT environment variable..."
 if [ -z "$SCRIPT" ]; then
   echo "SCRIPT environment variable is not set. Exiting."
   exit 1
 fi
 
-if [ -f "$SCRIPT" ]; then
-  python "$SCRIPT"
-else
+if [ ! -f "$SCRIPT" ]; then
   echo "Script file $SCRIPT does not exist. Exiting."
   exit 1
 fi
+
+OUTPUT_FILE="${SCRIPT}.out"
+
+# Step 3: Use salloc to request resources and execute the R script
+echo "===> [3/3] Request resources and run the R script"
+salloc -p standard --cpus-per-task="$CPUS" --time="$TIME" --mem="$MEM" -- bash -c "
+  # Load the R module
+  module load $R_MODULE
+
+  # Run the R script using R CMD BATCH
+  R CMD BATCH --no-save $SCRIPT $OUTPUT_FILE
+"
+echo "Output file path: $(realpath $OUTPUT_FILE)"
+echo "Output file size: $(du -h $OUTPUT_FILE | awk '{print $1}')"
+
+
+
